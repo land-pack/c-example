d02546218604c1ccf2ed8c2087703e45925b06ab
land-pack <landpack@sina.com>
2017-06-25 23:16:01 -0400

    [update] example

--- lua-extends/sample/hey.c
+++ lua-extends/sample/hey.c
@@ -18,13 +18,14 @@ int luaopen_hey(lua_State * L){
 	//lua_State *L = lua_open();
 	#if LUA_VERSION_NUM >= 502
 		luaL_newlib(L, heyfuncs);
-		//luaL_setfuncs(L, heyfuncs, 0);
-		//lua_pushvalue(L, -1);
-		//lua_pushcfunction(L, &hey);
-		//lua_setglobal(L, "hey");
-    	//luaL_register(L, "hey", heyfuncs);
+		
+		// Or second way
+		
+		/*
+		lua_newtable(L);
+		luaL_setfuncs(L, heyfuncs, 0);
+		*/
 	#else
-		//luaL_register(L, "ao", ao); /* 5.1 */
     	luaL_register(L, "hey", heyfuncs);
 	#endif
     return 1;
