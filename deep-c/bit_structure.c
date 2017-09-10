443fb99f75eee8e6171776b74932ebe0558b6b30
land-pack <landpack@sina.com>
2017-09-10 08:04:26 +0000

    add bit structure example

---
+++ deep-c/bit_structure.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+struct b_struct {
+	int a: 5;
+	int b: 2;
+	int c: 1;
+};
+
+int main(int argc, char *argv[])
+{
+
+	struct b_struct k;
+	k.a = 12;
+	k.b = 3;
+	k.c = 1;
+	return 0;
+}
+
